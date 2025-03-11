166518940427b478e013ccdef546780408bb3831
Vani Kuberappa <89070100+VaniKuberappa@users.noreply.github.com>
2025-03-11 15:55:04 +0100

    Update and rename 84.cpp to calculate_area_1.cpp

--- calculate_area_1.cpp
+++ calculate_area_1.cpp
@@ -10,7 +10,7 @@ int area_calculate(int r)
     {
         return pi*r*r;
     }
-int  area_calculate(int l,int b)
+int area_calculate(int l,int b)
     {
     return 2*(l+b);
     }
@@ -21,6 +21,5 @@ int main()
     area a;
     cout<<"area of circle"<<a.area_calculate(2)<<endl;
     cout<<"area of rectangle:"<<a.area_calculate(4,5)<<endl;
-
     return 0;
 }
