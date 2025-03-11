90753abdc2cddd66d44f729c6d37938ab3849f47
Vani Kuberappa <89070100+VaniKuberappa@users.noreply.github.com>
2025-03-11 15:49:23 +0100

    Update and rename 91.cpp to file_open.cpp

--- file_open.cpp
+++ file_open.cpp
@@ -2,14 +2,11 @@
 #include<fstream>
 using namespace std;
 
-
 int main()
 {
-
     ofstream t;
     t.open("text.txt");
     t<<"my first file handling example";
     t.close();
-
     return 0;
 }
